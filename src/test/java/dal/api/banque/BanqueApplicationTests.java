5f4027747d55e1583608b7ef415decfca4f647c6
hatimmrabet <mrabet.hatim2018@gmail.com>
2022-11-14 20:37:34 +0100

    utiliser mongo au lieu de notre cluster

--- src/test/java/dal/api/banque/BanqueApplicationTests.java
+++ src/test/java/dal/api/banque/BanqueApplicationTests.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import dal.api.banque.models.Account;
@@ -32,6 +33,7 @@ import dal.api.banque.services.StockService;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 @TestInstance(Lifecycle.PER_CLASS)
 class BanqueApplicationTests {
